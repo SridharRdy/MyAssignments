9d6eae07605f95073515cb0faa1e5ccae44c920d
SridharRdy <117844272+SridharRdy@users.noreply.github.com>
2022-11-09 21:26:59 +0530

    Add files via upload
    day1

---
+++ day1/Mobile.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Mobile {
+	
+	public void makeCall() {
+		// TODO Auto-generated constructor stub
+		System.out.println("i can login");
+	}
+	public void sendMsg() {
+		
+		System.out.println("This is my username");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Mobile myMobile = new Mobile();
+		
+		
+		myMobile.makeCall();
+		myMobile.sendMsg();
+		
+	}
+	
+
+}
